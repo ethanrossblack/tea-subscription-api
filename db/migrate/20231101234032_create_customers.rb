ee64702c050b89ee3e3db5df68fc859666b520af
Ethan Black <ethanrossblack@gmail.com>
2023-11-01 17:54:42 -0600

    feat: update schema to make customer columns required

--- db/migrate/20231101234032_create_customers.rb
+++ db/migrate/20231101234032_create_customers.rb
@@ -1,10 +1,10 @@
 class CreateCustomers < ActiveRecord::Migration[7.0]
   def change
     create_table :customers do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :email
-      t.string :address
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :address, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_01_234032) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_01_234652) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "customers", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "address"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "title", null: false
+    t.float "price", null: false
+    t.integer "frequency", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
