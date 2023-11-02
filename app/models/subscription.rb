8f442fa76f661ed9336eace5f62dd5c03a9e921d
Ethan Black <ethanrossblack@gmail.com>
2023-11-01 21:09:47 -0600

    feat: add validations and tests to models

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,7 @@
 class Subscription < ApplicationRecord
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :frequency, presence: true
+
+  enum :frequency, { monthly: 0, weekly: 1 }
 end
