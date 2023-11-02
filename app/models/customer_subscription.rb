0b2987afe70857be1cb2b22e46de000b60ddf189
Ethan Black <ethanrossblack@gmail.com>
2023-11-01 21:36:26 -0600

    feat: add customer_subscription model and tests/factories

---
+++ app/models/customer_subscription.rb
@@ -0,0 +1,10 @@
+class CustomerSubscription < ApplicationRecord
+  belongs_to :customer
+  belongs_to :subscription
+
+  enum :status, { active: 0, cancelled: 1 }
+
+  validates :customer_id, presence: true
+  validates :subscription_id, presence: true
+  validates :status, presence: true
+end
