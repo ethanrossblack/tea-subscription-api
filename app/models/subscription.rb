ab4113cccb3bc08b2c43bb2d61b1f3da52b0a1df
Ethan Black <ethanrossblack@gmail.com>
2023-11-01 21:54:36 -0600

    feat: add relationships to teas+subscriptions with subscription_teas

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,8 @@
 class Subscription < ApplicationRecord
   has_many :customer_subscriptions, dependent: :destroy
   has_many :customers, through: :customer_subscriptions
+  has_many :subscription_teas
+  has_many :teas, through: :subscription_teas
   
   enum :frequency, { monthly: 0, weekly: 1 }
   
